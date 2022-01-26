fc6b91f6620284fc6f8dac3250987b7e58a9c3c3
Florian Benscheidt <florian.benscheidt@ogd.nl>
2022-01-26 05:00:20 +0100

    2nd code init, off to bed now...

--- ctx.py
+++ ctx.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
+import argparse
+import random
+
 from conf import alphabet
 
 DEBUG = True
 
+
 if DEBUG:
-    print(alphabet[8])
+    print('Welcome to Debug hell!')
+    print(alphabet[random.randint(0,25)])
+
+
+def parser():
+    p = argparse.ArgumentParser()
+    p.add_argument('dev', help='Dev')
+    p.parse_args()
+
+
+def ctx():
+    p = parser()
+
+if __name__ == '__main__':
+    ctx()
