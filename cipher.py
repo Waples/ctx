ea638fa3cccdc395b078e465364d7e115d5b3d42
Florian Benscheidt <florian.benscheidt@ogd.nl>
2022-01-26 09:07:34 +0100

    encryption is working <3

--- cipher.py
+++ cipher.py
@@ -1,37 +1,56 @@
 import re
-from conf import alpha, key_len, rotations, num
+from itertools import cycle
+from conf import alpha_a, alpha_b, key_len, rotations, num
+from termcolor import cprint
 
 DEBUG = True
+if DEBUG:
+    MSG = 'Mijn code werkt gewoon!'
+    KEY = '538'
 
-def cnt(cipher, number):
-    ret = number+cipher
-    if ret > 25:
-        print('ERROR')
-    return res
+def converter(l, k=1):
+    """
+    Gets the corresponding number for the letter from the `alpha` dict, adds the
+    number with the `k` value and returns the corresponding letter.
+    """
+    initial_value = alpha_a[l]
+    new_value = initial_value + int(k)
+    if new_value > num:
+        new_value = new_value-num
+        if DEBUG:
+            cprint(f'converter ==> set to {new_value} == {alpha_b[new_value]}', 'yellow')
+    if DEBUG:
+        cprint(f'\t{l} =---> {alpha_b[new_value]}', 'cyan')
+    return alpha_b[new_value]
 
-def conv_num(l, k=1):
-    """Returns a number for given letter"""
-    i = alpha[l]
-    i = i + k
-    return i
 
-def conv_let(x):
-    """convert value of V to a letter"""
-    for k, v in alpha:
-        if v == x:
-            return k
+def sanitize(msg):
+    m =  re.sub(' ' , '', msg.upper())
+    ret = re.sub(r'[^\w]', '', m)
+    if DEBUG:
+        cprint(f'Sanitized: {ret}', 'blue')
+    return ret
 
 
 def encrypt(msg, key, rotor=rotations):
-    # convert to uppercase
-    m =  re.sub(' ' , '', msg.upper())
+    tmp = []
+    m = sanitize(msg)
+
+    # FIXME
+    #for _ in range(rotations):
+    key = cycle(key)
     for l in m:
-        l = conv_num(l)
-        ret = conv_let(l)
-        print(ret)
+        k = next(key)
+        tmp.append(converter(l, k))
+    return ''.join(tmp)
+
 
 def decrypt(key, rotor=rotations):
     pass
 
+
 if DEBUG:
-    encrypt(msg='hello world', key=538, rotor=0)
+    cprint(f'Initial msg: "{MSG}"', 'green')
+    cprint(f'Key: {KEY}', 'green')
+    e = encrypt(msg=MSG, key=KEY, rotor=0)
+    cprint(f'Encrypted output: {e}', 'white', 'on_green', attrs=['bold'])
