e52adad9e4f023aac381ac9ac6bf05e9e3066921
Florian Benscheidt <florian.benscheidt@ogd.nl>
2022-01-27 18:09:19 +0100

    pfft

--- ctx.py
+++ ctx.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python3
 import argparse
+import cipher
 import random
-
-from conf import alphabet
-
-DEBUG = True
-
-
-if DEBUG:
-    print('Welcome to Debug hell!')
-    print(alphabet[random.randint(0,25)])
 
 
 def parser():
     p = argparse.ArgumentParser()
-    p.add_argument('dev', help='Dev')
+    p.add_argument('-k', '--key', help='Cipher key.')
+    p.add_argument('-m', '--message', help='Your message.')
     p.parse_args()
 
 
 def ctx():
     p = parser()
+
 
 if __name__ == '__main__':
     ctx()
